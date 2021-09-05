e905b70ca58d5f94182b6a267bb291f6611456e8
Erepb-89 <erepb-89@yandex.ru>
2021-09-05 09:47:55 +0300

    update model product and product-category and added admin.py

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductsCategory(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=64)
     image = models.ImageField(upload_to='products_images', blank=True)
@@ -12,3 +15,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductsCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
