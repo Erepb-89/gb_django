f1532375a6a0c0778f335879d71f6a5cc1399062
Erepb-89 <erepb-89@yandex.ru>
2021-09-14 12:56:09 +0300

    added ajax handler

--- baskets/views.py
+++ baskets/views.py
@@ -3,6 +3,9 @@ from products.models import Product
 from baskets.models import Basket
 from django.contrib.auth.decorators import login_required
 
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 # Create your views here.
 @login_required
@@ -18,6 +21,24 @@ def baskets_add(request, id):
         baskets.quantity += 1
         baskets.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def baskets_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        context = {
+            'baskets': baskets
+        }
+        result = render_to_string('baskets/baskets.html', context)
+        return JsonResponse({'result': result})
 
 
 def baskets_remove(request, id):
