bf5adcc5d8a18224f5caec11ccbd27b8dee57000
Erepb-89 <erepb-89@yandex.ru>
2021-09-14 16:04:00 +0300

    added js ajax handler

--- static/js/baskets.js
+++ static/js/baskets.js
@@ -1,17 +1,41 @@
-window.onload = function () {
-    $('.basket_list').on('click', 'input[type="number"]', function () {
+// window.onload = function () {
+//     $('.basket_list').on('click', 'input[type="number"]', function () {
+//
+//         let t_href = event.target;
+//         console.log(t_href.name);
+//         console.log(t_href.value);
+//
+//         $.ajax({
+//             url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
+//             success: function (data) {
+//                 $('.basket_list').html(data.result)
+//             },
+//         });
+//         event.preventDefault();
+//     })
+// }
 
+window.onchange = function () {
+    let basketList = document.querySelector('.basket_list');
+    let inputs = document.querySelectorAll("[type='number']");
+
+    inputs.forEach(input => {
+        input.addEventListener('click', buttonClickHandler);
+    });
+
+    function buttonClickHandler(event) {
         let t_href = event.target;
-        console.log(t_href.name);
-        console.log(t_href.value);
 
         $.ajax({
             url: '/baskets/edit/' + t_href.name + '/' + t_href.value + '/',
             success: function (data) {
-                $('.basket_list').html(data.result)
-            },
+                basketList.innerHTML = '';
+                basketList.insertAdjacentHTML('afterbegin', data.result);
+            }
         });
         event.preventDefault();
-    })
-
+    }
 }
+
+
+
