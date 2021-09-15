0ade18ace1f97b590d2aa165aa41546faa757f56
Erepb-89 <erepb-89@yandex.ru>
2021-09-15 10:37:23 +0300

    changed admins app

--- admins/urls.py
+++ admins/urls.py
@@ -14,20 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+# from admins.views import baskets_add, baskets_remove, baskets_edit
 
-from products.views import index
+app_name = 'admins'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/',  include('products.urls', namespace='products')),
-    path('users/',  include('users.urls', namespace='users')),
-    path('baskets/',  include('baskets.urls', namespace='baskets')),
-    path('admins/',  include('admins.urls', namespace='admins'))
+    # path('baskets_add/<int:id>', baskets_add, name='baskets_add'),
+    # path('delete/<int:id>', baskets_remove, name='baskets_remove'),
+    # path('edit/<int:id>/<int:quantity>/', baskets_edit, name='baskets_edit'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
