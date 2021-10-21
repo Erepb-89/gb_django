92ae666ed3b41e69a8d2eda0af9bdc197d6b59d7
Erepb-89 <erepb-89@yandex.ru>
2021-10-21 23:23:06 +0300

    added cache

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -31,18 +31,21 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ {self.pk}'
 
-    @property
-    def _items(self):
-        return self.orderitems.select_related()
+    # @property
+    # def _items(self):
+    #     return self.orderitems.select_related()
 
     def get_total_quantity(self):
-        return sum(list(map(lambda x: x.quantity, self._items)))
+        items = self.orderitems.select_related()
+        return sum(list(map(lambda x: x.quantity, items)))
 
     def get_total_cost(self):
-        return sum(list(map(lambda x: x.get_product_cost(), self._items)))
+        items = self.orderitems.select_related()
+        return sum(list(map(lambda x: x.get_product_cost(), items)))
 
     def delete(self, using=None, keep_parents=False):
-        for item in self._items:
+        items = self.orderitems.select_related()
+        for item in items:
             item.product.quantity += item.quantity
             item.product.save()
         self.is_active = False
