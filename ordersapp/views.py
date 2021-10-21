8205565e01a34c76fd89e62cd9e7255af1b88561
Erepb-89 <erepb-89@yandex.ru>
2021-10-21 19:56:36 +0300

    added cache

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -128,21 +128,21 @@ def order_forming_complete(request, pk):
     return HttpResponseRedirect(reverse('orders:list'))
 
 
-@receiver(pre_save, sender=Basket)
-@receiver(pre_save, sender=OrderItem)
-def product_quantity_update_delete(sender, instance, **kwargs):
-    if instance.pk:
-        instance.product.quantity -= instance.quantity - instance.get_item(int(instance.pk))
-    else:
-        instance.product.quantity -= instance.quantity
-    instance.product.save()
-
-
-@receiver(pre_delete, sender=Basket)
-@receiver(pre_delete, sender=OrderItem)
-def product_quantity_update_delete(sender, instance, **kwargs):
-    instance.product.quantity += instance.quantity
-    instance.product.save()
+# @receiver(pre_save, sender=Basket)
+# @receiver(pre_save, sender=OrderItem)
+# def product_quantity_update_delete(sender, instance, **kwargs):
+#     if instance.pk:
+#         instance.product.quantity -= instance.quantity - instance.get_item(int(instance.pk))
+#     else:
+#         instance.product.quantity -= instance.quantity
+#     instance.product.save()
+#
+#
+# @receiver(pre_delete, sender=Basket)
+# @receiver(pre_delete, sender=OrderItem)
+# def product_quantity_update_delete(sender, instance, **kwargs):
+#     instance.product.quantity += instance.quantity
+#     instance.product.save()
 
 
 def get_product_price(request, pk):
