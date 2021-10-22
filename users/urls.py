f17171d740d8398d0fb16b177ed15be8df69210c
Erepb-89 <erepb-89@yandex.ru>
2021-10-22 16:55:06 +0300

    added cache

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from users.views import LoginListView, RegisterListView, ProfileFormView, logout\
-    #Logout
+from users.views import LoginListView, RegisterListView, ProfileFormView, Logout
 
 app_name = 'users'
 
@@ -8,6 +7,6 @@ urlpatterns = [
     path('login/', LoginListView.as_view(), name='login'),
     path('register/', RegisterListView.as_view(), name='register'),
     path('profile/', ProfileFormView.as_view(), name='profile'),
-    path('logout/', logout, name='logout'),
+    path('logout/', Logout.as_view(), name='logout'),
     path('verify/<str:email>/<str:activation_key>/', RegisterListView.verify, name='verify')
 ]
