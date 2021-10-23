f83b61704e4c83b95a8cc3bf2d890bf92fd1fb7e
Erepb-89 <erepb-89@yandex.ru>
2021-10-23 14:25:12 +0300

    added cache

--- products/views.py
+++ products/views.py
@@ -71,7 +71,7 @@ class ProductDetailView(DetailView):
 
 
 # @never_cache
-@cache_page(3600)
+# @cache_page(3600)
 def products(request, id=None, page=1):
     products = Product.objects.filter(category_id=id).select_related(
         'category') if id is not None and id != 0 else Product.objects.all().select_related()
